496cfc08b2ce1a51e76501285c44a2e8b8eb3c4a
mohnapne <kuvaldin2012@gmail.com>
2024-11-07 09:58:58 +0100

    init

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Lesson05 from "./lessons/lesson05/Lesson05";
+//import Homework04 from "./homeworks/homework04/Homework04";
+// import Lesson01 from "./lessons/lesson01/Lesson01";
+// import Lesson03 from "./lessons/lesson03/Lesson03";
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {/* <Lesson01/> */}
+    {/* <Lesson03/> */}
+    {/* <Lesson04/> */}
+    {/* <Homework04/> */}
+    <Lesson05/>
+  </>
+);
